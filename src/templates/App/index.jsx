6a6e826cf43926cd10c51c4b2f829cefac2daadd
Konstructa <elenprojects7@gmail.com>
2023-05-24 15:38:43 -0300

    Boilerplate

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header" style={{ textAlign: 'center' }}>
+        <p>Hello</p>
+      </header>
+    </div>
+  );
+};
