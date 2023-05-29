7801f84dfe0ad2e98c2060bb0102c3c130426fc5
Konstructa <elenprojects7@gmail.com>
2023-05-29 14:43:49 -0300

    update

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,23 @@
-/** @type { import('@storybook/react').Preview } */
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from '../src/styles/global-styles'
-import { theme } from '../src/styles/theme'
+import { GlobalStyles } from '../src/styles/global-styles';
+import { theme } from '../src/styles/theme';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.primaryColor,
+      },
+    ],
+  },
+};
 
 export const decorators = [
   (Story) => (
@@ -13,5 +25,5 @@ export const decorators = [
       <Story />
       <GlobalStyles />
     </ThemeProvider>
-  )
+  ),
 ];
